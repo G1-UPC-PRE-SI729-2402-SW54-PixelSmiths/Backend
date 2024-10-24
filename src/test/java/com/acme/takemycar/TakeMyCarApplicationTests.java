7d6d98d7f120a252746dc2757fe8610bf5a207cf
Alvaro <129535538+AlvaroPFR@users.noreply.github.com>
2024-10-24 18:32:54 -0500

    chore: initial commit

---
+++ src/test/java/com/acme/takemycar/TakeMyCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.takemycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeMyCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
